449f2ef9fb657f1ccb28ee7cb8088a41d357dd5d
tarql24 <tarql24@gmail.com>
2019-06-21 20:16:45 -0700

    added more code

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,22 +1,22 @@
-var friends = [
+var friendsArray = [
   {
     name: 'Yoda',
     photo:
-      'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg',
+      'https://spacequotations.com/wp-content/uploads/2018/09/yoda-810x456.jpg',
     scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
   },
   {
-    name: 'Darthmal',
+    name: 'Darth Maul',
     photo:
-      'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg',
+      'https://i.pinimg.com/originals/6e/03/6e/6e036ef56f308b078713cebbab46a3a4.jpg',
     scores: [5, 2, 4, 2, 1, 1, 4, 1, 4, 5]
   },
   {
     name: 'Darth Vador',
     photo:
-      'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg',
+      'https://cdn.flickeringmyth.com/wp-content/uploads/2018/05/static1.squarespace.jpg',
     scores: [1, 3, 4, 4, 2, 1, 4, 5, 3, 4]
   }
 ];
 
-module.export = friends;
+module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,11 @@
 // ===============================================================================
 // LOAD DATA
 // We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
+// These data sources hold arrays of information on friends data.
 // ===============================================================================
-
-var tableData = require('../data/tableData');
-var waitListData = require('../data/waitinglistData');
+var path = require('path');
+var friendsArray = require('../data/friends.js');
+// var waitListData = require('../data/waitinglistData');
 
 // ===============================================================================
 // ROUTING
@@ -19,43 +19,67 @@ module.exports = function(app) {
   // ---------------------------------------------------------------------------
 
   app.get('/api/friends', function(req, res) {
-    res.json(friends);
+    res.json(friendsArray);
   });
-
-  //   app.get('/api/waitlist', function(req, res) {
-  //     res.json(waitListData);
-  //   });
-
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
 
   app.post('/api/friends', function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body parsing middleware
-    if (friends.length < 100) {
-      friends.push(req.body);
-      res.json(true);
-    } else {
-      waitListData.push(req.body);
-      res.json(false);
-    }
-  });
+    var newFriend = {
+      name: req.body.name,
+      photo: req.body.photo,
+      scores: JSON.parse(req.body.scores)
+    };
 
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
+    //   var diffArray = [];
 
-  app.post('/api/clear', function(req, res) {
-    // Empty out the arrays of data
-    tableData.length = [];
-    waitListData.length = [];
+    //   userArray.forEach(function(item, index) {
+    //     var difference = 0;
+    //     for (var i = 0; i < item.scores.length; i++) {
+    //       difference += Math.abs(item.scores[i] - newPerson.scores[i]);
+    //     }
+    //     diffArray.push({ difference: difference, index: index });
+    //   });
 
-    res.json({ ok: true });
+    //   diffArray.sort(function(a, b) {
+    //     return a.difference - b.difference;
+    //   });
+
+    friendsArray.push(newFriend);
+    //   res.json(userArray[diffArray[0].index]);
   });
 };
+//   app.get('/api/waitlist', function(req, res) {
+//     res.json(waitListData);
+//   });
+
+// API POST Requests
+// Below code handles when a user submits a form and thus submits data to the server.
+// In each of the below cases, when a user submits form data (a JSON object)
+// ...the JSON is pushed to the appropriate JavaScript array
+// (ex. User fills out a reservation request... this data is then sent to the server...
+// Then the server saves the data to the tableData array)
+// ---------------------------------------------------------------------------
+
+// app.post('/api/friends', function(req, res) {
+//   // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+//   // It will do this by sending out the value "true" have a table
+//   // req.body is available since we're using the body parsing middleware
+//   if (friends.length < 100) {
+//     friends.push(req.body);
+//     res.json(true);
+//   } else {
+//     waitListData.push(req.body);
+//     res.json(false);
+//   }
+// });
+
+// // ---------------------------------------------------------------------------
+// // I added this below code so you could clear out the table while working with the functionality.
+// // Don"t worry about it!
+
+// app.post('/api/clear', function(req, res) {
+//   // Empty out the arrays of data
+//   tableData.length = [];
+//   waitListData.length = [];
+
+//   res.json({ ok: true });
+// });
