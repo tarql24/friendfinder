5dd78c88fdfdbf11ca24d53de22356d14a619253
tarql24 <tarql24@gmail.com>
2019-06-21 19:07:30 -0700

    added all base files

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+var friends = [
+  {
+    name: 'Yoda',
+    photo:
+      'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg',
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: 'Darthmal',
+    photo:
+      'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg',
+    scores: [5, 2, 4, 2, 1, 1, 4, 1, 4, 5]
+  },
+  {
+    name: 'Darth Vador',
+    photo:
+      'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg',
+    scores: [1, 3, 4, 4, 2, 1, 4, 5, 3, 4]
+  }
+];
+
+module.export = friends;
---
+++ server.js
@@ -0,0 +1,36 @@
+// Dependencies
+// =============================================================
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8000;
+
+// Sets up the Express app to handle data parsing
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('app'));
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users
+// visit or request data from various URLs.
+// ================================================================================
+
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
+// require('./routes/apiRoutes')(app);
+// require('./routes/htmlRoutes')(app);
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log('App listening on PORT: ' + PORT);
+});
